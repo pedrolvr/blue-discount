128d78c43a9079629b2b71e59b4ce0e09525da13
Pedro Oliveira <phenrigomes@gmail.com>
2020-09-16 01:02:22 -0300

    Fix image name

--- deploy/build-push-image.sh
+++ deploy/build-push-image.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 COMMIT_ID="$(git rev-parse --short $CIRCLE_SHA1)"
-IMAGE_NAME=phenrigomes/lemon-discount:$CIRCLE_BRANCH-$COMMIT_ID
+IMAGE_NAME=phenrigomes/blue-discount:$CIRCLE_BRANCH-$COMMIT_ID
 docker build -t $IMAGE_NAME .
 docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
 docker push $IMAGE_NAME
