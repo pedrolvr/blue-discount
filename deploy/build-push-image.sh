0ac68744d24167ce16c2d652433c16cdaf8c368e
Pedro Oliveira <phenrigomes@gmail.com>
2020-09-16 01:18:51 -0300

    Remove warning on docker login

--- deploy/build-push-image.sh
+++ deploy/build-push-image.sh
@@ -5,5 +5,5 @@ set -eo pipefail
 COMMIT_ID="$(git rev-parse --short $CIRCLE_SHA1)"
 IMAGE_NAME=phenrigomes/blue-discount:$CIRCLE_BRANCH-$COMMIT_ID
 docker build -t $IMAGE_NAME .
-docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
+echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 docker push $IMAGE_NAME
